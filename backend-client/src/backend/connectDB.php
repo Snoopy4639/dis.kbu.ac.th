3aead80284e09134bce65998d91f4f0dcb90f342
Mr.Book <jirayu.oopi@gmail.com>
2018-10-18 22:51:54 +0700

    Add command for DIS Server DB !

--- backend-client/src/backend/connectDB.php
+++ backend-client/src/backend/connectDB.php
@@ -1,5 +1,6 @@
 <?php
     $conn = mysqli_connect('localhost', 'u664292652_root', 'password', 'u664292652_dis') or die ('Error'.mysqli_connect_error());
+    // $conn = mysqli_connect('localhost', 'root', 'password', 'dis') or die ('Error'.mysqli_connect_error());
     // $conn = mysqli_connect('localhost', 'root', '', 'dis') or die ('Error'.mysqli_connect_error());
     mysqli_set_charset($conn,"utf8");
 ?>
